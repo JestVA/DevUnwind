adc9281a974bbf0e73c4930620e69595905d999d
Dorin Stefan <d.dumitrascuta@gmail.com>
2021-03-11 13:29:19 +0000

    Add unhealthy to healthy feelings map - they can still be negative - not all negative feelings are bad!
    Actually some of them keep us safe

---
+++ 1-intro-to-rebt-for-highly-performing-developers/healthyToUnhealthyFeelingsMap.js
@@ -0,0 +1,11 @@
+const whenIFeel = {
+	"extreme panic": "concern",
+	"angry": "displeased",
+	"rage": "care",
+	"anxiety": "vigilance",
+	"over concern": "caution"
+};
+
+whenIFeel["extreme panic"]; // I will choose to be "concerned"
+whenIFeel["angry"] // I will choose to be "displeased"
+
